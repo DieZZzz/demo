67062a7e8023f882561c518365c29f6d54829988
Siarhei Yeharetski <Siarhei_Yeharetski@epam.com>
2017-07-26 09:31:24 +0300

    added CommitService as a dependency to KafkaKonsumerService

--- app-kafka-consumer/src/main/java/com/epam/demo/service/KafkaConsumerServiceImpl.java
+++ app-kafka-consumer/src/main/java/com/epam/demo/service/KafkaConsumerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.epam.demo.service;
 
 import com.epam.demo.model.Commit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,14 @@ import java.util.concurrent.CountDownLatch;
 @Service
 public class KafkaConsumerServiceImpl implements KafkaConsumerService {
 
+    private final CommitService commitService;
+
     private CountDownLatch latch = new CountDownLatch(1);
+
+    @Autowired
+    public KafkaConsumerServiceImpl(CommitService commitService) {
+        this.commitService = commitService;
+    }
 
     public CountDownLatch getLatch() {
         return latch;
@@ -19,5 +27,6 @@ public class KafkaConsumerServiceImpl implements KafkaConsumerService {
     @KafkaListener(topics = "${kafka.topic}")
     public void receive(Commit message) {
         latch.countDown();
+        commitService.save(message);
     }
 }
